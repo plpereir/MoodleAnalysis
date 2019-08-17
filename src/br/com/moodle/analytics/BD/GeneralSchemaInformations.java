b8bddcac3013611946ab61caa8cb658ea341fb54
Pedro <Pedro@pl.silvapereira@gmail.com>
2019-08-17 02:45:20 -0300

    update GeneralSchemaInformations class with new items.

--- src/br/com/moodle/analytics/BD/GeneralSchemaInformations.java
+++ src/br/com/moodle/analytics/BD/GeneralSchemaInformations.java
@@ -3,32 +3,70 @@
  */
 package br.com.moodle.analytics.BD;
 
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author Pedro
- * The proposal this class is provide relevant informations
- * about the schema, for example return the list of modules used this 
- * moodle instance
+ * @author Pedro The proposal this class is provide relevant informations about
+ *         the schema, for example return the list of modules used this moodle
+ *         instance
  */
 public class GeneralSchemaInformations {
 
-	ConnectionFactory cf = new ConnectionFactory();
-	
-	
-	private List<String> getAllTablesFromSchema()
-	{
+	private static List<String> getAllTablesFromSchema() {
 		List<String> tables = new ArrayList<>();
+
+		DatabaseMetaData md;
+		try {
+			md = ConnectionFactory.getConnectionMySQL().getMetaData();
+			ResultSet rs = md.getTables(null, null, "%", null);
+			while (rs.next()) {
+				if (ConnectionFactory.prop.getPropertyValue("mydatabase").equals(rs.getString(1))
+						&& rs.getString(4).equals("TABLE")) {
+					tables.add(rs.getString(3));
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return tables;
 	}
-	
-	
+
+	  public static void getColumnNames(String table) throws SQLException {
+		     DatabaseMetaData meta = ConnectionFactory.getConnectionMySQL().getMetaData();
+		     ResultSet res = meta.getColumns(ConnectionFactory.prop.getPropertyValue("mydatabase"), null, table, "%");
+		      System.out.println("List of columns: "); 
+		      while (res.next()) {
+		         System.out.println(
+		           "  "+res.getString("TABLE_SCHEM")
+		           + ", "+res.getString("TABLE_NAME")
+		           + ", "+res.getString("COLUMN_NAME")
+		           + ", "+res.getString("TYPE_NAME")
+		           + ", "+res.getInt("COLUMN_SIZE")
+		           + ", "+res.getInt("NULLABLE")); 
+		      }
+		      res.close();
+
+		  }
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Matheus GamerPro");
+	/*for (String table : getAllTablesFromSchema()) {
+			System.out.println(table);
+		}*/
+		try {
+			getColumnNames("mdl_assign");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
